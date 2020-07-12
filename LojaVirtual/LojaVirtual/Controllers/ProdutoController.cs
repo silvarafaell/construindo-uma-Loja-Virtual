2d8a38c2ba8941867acb3a170d43dfd380efac8d
francisco rafael <francisco_rafael@hotmail.com.br>
2020-07-11 23:36:52 -0300

    Aula 44 - Usando Model

--- LojaVirtual/LojaVirtual/Controllers/ProdutoController.cs
+++ LojaVirtual/LojaVirtual/Controllers/ProdutoController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LojaVirtual.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,10 +17,25 @@ namespace LojaVirtual.Controllers
 
         public ActionResult Visualizar()
         {
+            Produto produto = GetProduto();
 
-            return View();
+            return View(produto);  //passar o produto para a tela
            //return new ContentResult() { Content = "<h3>Produto Visualizar<h3>", ContentType = "text/html" };
             
         }
+
+        private Produto GetProduto()
+        {
+            return new Produto()
+            {
+                Id = 1,
+                Nome = "Notebook para Programar",
+                Descricao = "Programar mais",
+                Valor = 4000.00M
+
+            };
+
+        }
+
     }
 }
