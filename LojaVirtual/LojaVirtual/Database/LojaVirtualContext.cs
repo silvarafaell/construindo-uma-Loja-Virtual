466501c3c5a2f54798d74d1e2c16e89ec54d3321
francisco rafael <francisco_rafael@hotmail.com.br>
2020-07-19 19:18:11 -0300

    configuração do banco de dados - parte1

---
+++ LojaVirtual/LojaVirtual/Database/LojaVirtualContext.cs
@@ -0,0 +1,25 @@
+﻿using LojaVirtual.Models;
+using Microsoft.EntityFrameworkCore; //referente ao banco
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Database
+{
+    public class LojaVirtualContext : DbContext
+    {
+        /* EF - Core
+         * SQL >
+         * ORM > Biblioteca mapear objetos para Banco de Dados relacionais
+         */
+        
+        //Construtor
+        public LojaVirtualContext(DbContextOptions<LojaVirtualContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
