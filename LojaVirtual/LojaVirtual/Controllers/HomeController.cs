ff390645be517ee5f5b47c283a69a5880e8bdd89
francisco rafael <francisco_rafael@hotmail.com.br>
2020-09-25 22:24:28 -0300

    Aula 67 - Newslettter - HTML no formulário

--- LojaVirtual/LojaVirtual/Controllers/HomeController.cs
+++ LojaVirtual/LojaVirtual/Controllers/HomeController.cs
@@ -14,7 +14,8 @@ namespace LojaVirtual.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var news = new NewsletterEmail() { Email = "francisco.rafaell.pereira@gmail.com" };
+            return View(news);
         }
 
         public IActionResult Contato()
