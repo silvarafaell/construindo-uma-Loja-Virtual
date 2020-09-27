7001f3369484302859ee513601d9f6e58589cf1d
francisco rafael <francisco_rafael@hotmail.com.br>
2020-09-27 15:23:58 -0300

    aula 68 - Newsletter -Fromform e HttpGet e HttpPost

--- LojaVirtual/LojaVirtual/Controllers/HomeController.cs
+++ LojaVirtual/LojaVirtual/Controllers/HomeController.cs
@@ -12,10 +12,19 @@ namespace LojaVirtual.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
-            var news = new NewsletterEmail() { Email = "francisco.rafaell.pereira@gmail.com" };
-            return View(news);
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index([FromForm] NewsletterEmail newsletter)
+        {
+            //TODO - Adição no banco de dados
+
+            //TODO - Validações
+            return View();
         }
 
         public IActionResult Contato()
