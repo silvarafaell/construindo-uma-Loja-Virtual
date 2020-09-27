89ef575afb9931b2e8be5c2093984727d0c5a2d3
francisco rafael <francisco_rafael@hotmail.com.br>
2020-09-27 19:56:46 -0300

    aula 70 - Newsletter -Salvando os dados no banco

--- LojaVirtual/LojaVirtual/Controllers/HomeController.cs
+++ LojaVirtual/LojaVirtual/Controllers/HomeController.cs
@@ -7,11 +7,17 @@ using LojaVirtual.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
+using LojaVirtual.Database;
 
 namespace LojaVirtual.Controllers
 {
     public class HomeController : Controller
     {
+        private LojaVirtualContext _banco;
+        public HomeController(LojaVirtualContext banco)
+        {
+            _banco = banco;
+        }
         [HttpGet]
         public IActionResult Index()
         {
@@ -23,7 +29,11 @@ namespace LojaVirtual.Controllers
         {
             if (ModelState.IsValid)
             {
-                //TODO - Adição no banco de dados
+                _banco.NewsletterEmails.Add(newsletter);
+                _banco.SaveChanges();
+
+                TempData["MSG_S"] = "E-mail cadastrado! Agora você vai receber promoções especiais no seu e-mail";
+
                 return RedirectToAction(nameof(Index));
             }
             else
