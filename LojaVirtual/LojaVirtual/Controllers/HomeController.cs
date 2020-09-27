05a2f47808dec0eda729d8c04af10165d0f2fb2c
francisco rafael <francisco_rafael@hotmail.com.br>
2020-09-27 15:53:33 -0300

    aula 69 - Newsletter -Validação com ASP.NET

--- LojaVirtual/LojaVirtual/Controllers/HomeController.cs
+++ LojaVirtual/LojaVirtual/Controllers/HomeController.cs
@@ -21,10 +21,15 @@ namespace LojaVirtual.Controllers
         [HttpPost]
         public IActionResult Index([FromForm] NewsletterEmail newsletter)
         {
-            //TODO - Adição no banco de dados
-
-            //TODO - Validações
-            return View();
+            if (ModelState.IsValid)
+            {
+                //TODO - Adição no banco de dados
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View();
+            }
         }
 
         public IActionResult Contato()
--- LojaVirtual/LojaVirtual/Models/NewsletterEmail.cs
+++ LojaVirtual/LojaVirtual/Models/NewsletterEmail.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using LojaVirtual.Libraries.Lang;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +10,9 @@ namespace LojaVirtual.Models
     public class NewsletterEmail
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E001")] //deixa o campo obrigatorio
+        [EmailAddress(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E004")] //validacao especifica do email
         public string Email { get; set; }
     }
 }
