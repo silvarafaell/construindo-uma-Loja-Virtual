67d34c3678d024bc76a7ef3237040abeeeea654d
francisco rafael <francisco_rafael@hotmail.com.br>
2020-11-28 22:16:15 -0300

    Aula 105 - Area Colaborador - Paginacao Categoria

--- LojaVirtual/LojaVirtual/Areas/Colaborador/Controllers/CategoriaController.cs
+++ LojaVirtual/LojaVirtual/Areas/Colaborador/Controllers/CategoriaController.cs
@@ -6,6 +6,7 @@ using LojaVirtual.Libraries.Filtro;
 using LojaVirtual.Models;
 using LojaVirtual.Repositories.Contracts;
 using Microsoft.AspNetCore.Mvc;
+using X.PagedList;
 
 namespace LojaVirtual.Areas.Colaborador.Controllers
 {
@@ -21,9 +22,10 @@ namespace LojaVirtual.Areas.Colaborador.Controllers
             _categoriaRepository = categoriaRepository;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int? pagina)
         {
-            List<Categoria> categorias = _categoriaRepository.ObterTodasCategorias().ToList();
+            //
+            var categorias = _categoriaRepository.ObterTodasCategorias(pagina);
             return View(categorias);
         }
 
