c88e2238c0551450e7ba8971cdd044c522ba4201
francisco rafael <francisco_rafael@hotmail.com.br>
2020-12-03 19:37:43 -0300

    Aula 108 - Area Colaborador - Categoria Pai

--- LojaVirtual/LojaVirtual/Repositories/CategoriaRepository.cs
+++ LojaVirtual/LojaVirtual/Repositories/CategoriaRepository.cs
@@ -1,6 +1,7 @@
 ﻿using LojaVirtual.Database;
 using LojaVirtual.Models;
 using LojaVirtual.Repositories.Contracts;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -45,7 +46,7 @@ namespace LojaVirtual.Repositories
         public IPagedList<Categoria> ObterTodasCategorias(int? pagina)
         {
             int NumeroPagina = pagina ?? 1;
-            return _banco.Categorias.ToPagedList<Categoria>(NumeroPagina, _registroPorPagina);
+            return _banco.Categorias.Include(a=>a.categoriaPai).ToPagedList<Categoria>(NumeroPagina, _registroPorPagina);
         }
     }
 }
