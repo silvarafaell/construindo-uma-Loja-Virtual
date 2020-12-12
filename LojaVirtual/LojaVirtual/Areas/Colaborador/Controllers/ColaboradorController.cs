0993b10fed94cb2a432ac14e1e80cc18b627b1b2
Francisco Rafael <francisco_rafael@hotmail.com.br>
2020-12-12 13:15:45 -0300

    Aula 115 - Area Colaborador - Classe Colaborador e Controller

---
+++ LojaVirtual/LojaVirtual/Areas/Colaborador/Controllers/ColaboradorController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Areas.Colaborador.Controllers
+{
+    public class ColaboradorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar([FromForm]Models.Colaborador colaborador)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Atualizar(int id)
+        {
+            return View();
+        }
+    }
+}
--- LojaVirtual/LojaVirtual/Models/Colaborador.cs
+++ LojaVirtual/LojaVirtual/Models/Colaborador.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using LojaVirtual.Libraries.Lang;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,15 +10,30 @@ namespace LojaVirtual.Models
 {
     public class Colaborador
     {
+        [Display(Name = "Código")]
         public int Id { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E001")] //deixa o campo obrigatorio
+        [MinLength(4, ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E002")] //quantidade minima de caracteres
         public string Nome{ get; set; }
+
+        [Display(Name ="E-mail")]
+        [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E001")] //deixa o campo obrigatorio
+        [EmailAddress(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E004")] //validacao especifica do email
         public string Email { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E001")] //deixa o campo obrigatorio
+        [MinLength(6, ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E002")] //quantidade minima de caracteres
         public string Senha { get; set; }
+
+        [NotMapped] //Não velar o campo para o Banco
+        public string ConfirmacaoSenha { get; set; }
         /*
          * TIPO
          * C - Comum
          * G - Gerente
          */
+        [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E001")] //deixa o campo obrigatorio
         public string Tipo { get; set; }
     }
 }
