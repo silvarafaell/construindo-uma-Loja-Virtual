90bb9c6690ab5d116a43f7e291c76d244a4a8831
Francisco Rafael <francisco_rafael@hotmail.com.br>
2020-12-12 07:22:26 -0300

    Aula 111 - Area Colaborador - Atualizar Categoria

--- LojaVirtual/LojaVirtual/Areas/Colaborador/Controllers/CategoriaController.cs
+++ LojaVirtual/LojaVirtual/Areas/Colaborador/Controllers/CategoriaController.cs
@@ -56,12 +56,24 @@ namespace LojaVirtual.Areas.Colaborador.Controllers
         [HttpGet]
         public IActionResult Atualizar(int id)
         {
-            return View();
+            var categoria = _categoriaRepository.ObterCategoria(id);
+            ViewBag.Categorias = _categoriaRepository.ObterTodasCategorias().Where(a=>a.Id != id).Select(a => new SelectListItem(a.Nome, a.Id.ToString()));
+
+            return View(categoria);
         }
 
         [HttpPost]
-        public IActionResult Atualizar([FromForm]Categoria categoria)
+        public IActionResult Atualizar([FromForm]Categoria categoria, int id)
         {
+            if(ModelState.IsValid)
+            {
+                _categoriaRepository.Atualizar(categoria);
+
+                TempData["MSG_S"] = "Registro salvo com sucesso";
+
+                return RedirectToAction(nameof(Index));
+            }
+            ViewBag.Categorias = _categoriaRepository.ObterTodasCategorias().Where(a => a.Id != id).Select(a => new SelectListItem(a.Nome, a.Id.ToString()));
             return View();
         }
 
