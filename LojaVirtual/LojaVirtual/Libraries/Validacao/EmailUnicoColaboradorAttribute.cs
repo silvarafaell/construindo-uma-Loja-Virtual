59e48889ce700d253467f6f42e034f9ad5d748d9
Francisco Rafael <francisco_rafael@hotmail.com.br>
2020-12-18 18:21:20 -0300

    Aula 130 - Area Colaborador - Email unico validacao - parte 2

--- LojaVirtual/LojaVirtual/Libraries/Validacao/EmailUnicoColaboradorAttribute.cs
+++ LojaVirtual/LojaVirtual/Libraries/Validacao/EmailUnicoColaboradorAttribute.cs
@@ -12,12 +12,24 @@ namespace LojaVirtual.Libraries.Validacao
     {
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
-            string Email = value as string;
+            string Email = (value as string).Trim();
 
             IColaboradorRepository _colaboradorRepository = (IColaboradorRepository) validationContext.GetService(typeof(IColaboradorRepository));
-            List<Colaborador> colaboradors = _colaboradorRepository.ObterColaboradorPorEmail(Email);
+            List<Colaborador> colaboradores = _colaboradorRepository.ObterColaboradorPorEmail(Email);
 
-            return base.IsValid(value, validationContext);
+            Colaborador objColaborador = (Colaborador)validationContext.ObjectInstance;
+
+            //Rejeitar
+            if(colaboradores.Count > 1)
+            {
+                return new ValidationResult("E-mail já existente!");
+            }
+            if(colaboradores.Count == 1 && objColaborador.Id != colaboradores[0].Id)
+            {
+                return new ValidationResult("E-mail já existente!");
+            }
+
+            return ValidationResult.Success;
         }
     }
 }
