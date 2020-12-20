d83be3f491e7a07d8ae567ab525867a0bbb83a9a
Francisco Rafael <francisco_rafael@hotmail.com.br>
2020-12-20 00:00:03 -0300

    Aula 141 - Area Colaborador - Migration Cliente - Adicionar Situação

--- LojaVirtual/LojaVirtual/Models/Cliente.cs
+++ LojaVirtual/LojaVirtual/Models/Cliente.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -35,6 +36,14 @@ namespace LojaVirtual.Models
         [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E001")] //deixa o campo obrigatorio
         [MinLength(6, ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E002")] //quantidade minima de caracteres
         public string Senha { get; set; }
+
+        [NotMapped] //Não velar o campo para o Banco
+        [Display(Name = "Confirme a senha")]
+        [Compare("Senha", ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E005")]
+        public string ConfirmacaoSenha { get; set; }
+
+        [Display(Name = "Situação")]
+        public string Situacao { set; get; }
 
 
     }   
