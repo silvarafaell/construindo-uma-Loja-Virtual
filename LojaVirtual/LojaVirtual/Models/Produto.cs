111fd92876d68a6d69cf7509927bc3e347d0ad4d
Francisco Rafael P. da Silva <francisco.silva@fitbank.com.br>
2020-12-21 11:29:05 -0300

    Aula 146 - Area Colaborador - Modelo Produto

--- LojaVirtual/LojaVirtual/Models/Produto.cs
+++ LojaVirtual/LojaVirtual/Models/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,5 +14,22 @@ namespace LojaVirtual.Models
         public string Nome { get; set; }
         public string Descricao { get; set; }
         public decimal Valor { get; set; }
+        public int Quantidade { get; set; }
+
+        //Frete - Correios
+        public double Peso { get; set; }
+        public int Largura { get; set; }
+        public int Altura { get; set; }
+        public int Comprimento { get; set; }
+
+        //Banco de dados - Relacionamento entre Tabela
+        public int CategoriaId { get; set; }
+
+        //POO - Associações entre Objetos
+        [ForeignKey("CategoriaId")]
+        public Categoria categoria { get; set; }
+
+        //EF - ORM - Biblioteca Unir - Banco de dados e POO.(ORM - Mapeamento de Objetos <-> Relacionamento)
+        //Fluente API - Attributes
     }
 }
