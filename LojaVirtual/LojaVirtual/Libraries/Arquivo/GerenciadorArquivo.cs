ef079e8c7dd1ad10105156582557fd7c7f203c0e
Francisco Rafael <francisco_rafael@hotmail.com.br>
2020-12-26 09:30:43 -0300

    Aula 164 - Upload - Gerenciador Arquivo parte2

--- LojaVirtual/LojaVirtual/Libraries/Arquivo/GerenciadorArquivo.cs
+++ LojaVirtual/LojaVirtual/Libraries/Arquivo/GerenciadorArquivo.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,16 @@ namespace LojaVirtual.Libraries.Arquivo
 {
     public class GerenciadorArquivo
     {
-        public static void CadastrarImagemProduto(IFormFile file)
+        public static string CadastrarImagemProduto(IFormFile file)
         {
+            var NomeArquivo = Path.GetFileName(file.FileName);
+            var Caminho = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/uploads/temp", NomeArquivo);
 
+            using (var stream = new FileStream(Caminho, FileMode.Create))
+            {
+                file.CopyTo(stream);
+            }
+            return Path.Combine("/uploads/temp", NomeArquivo);
         }
 
         public static void ExcluirImagemProduto()
