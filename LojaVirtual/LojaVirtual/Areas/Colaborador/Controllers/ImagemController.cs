40c61e82d3c4f17d6cea586fa1926f21819efe0d
Francisco Rafael <francisco_rafael@hotmail.com.br>
2020-12-26 08:55:47 -0300

    Aula 163 - Upload - Controlador ImagemController e GerenciadorArquivo Parte1

---
+++ LojaVirtual/LojaVirtual/Areas/Colaborador/Controllers/ImagemController.cs
@@ -0,0 +1,25 @@
+﻿using LojaVirtual.Libraries.Arquivo;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Areas.Colaborador.Controllers
+{
+    public class ImagemController : Controller
+    {
+        [Area("Colaborador")]
+        public IActionResult Armazenar(IFormFile file)
+        {
+            GerenciadorArquivo.CadastrarImagemProduto(file);
+            return View();
+        }
+
+        public IActionResult Deletar()
+        {
+            return View();
+        }
+    }
+}
---
+++ LojaVirtual/LojaVirtual/Libraries/Arquivo/GerenciadorArquivo.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Libraries.Arquivo
+{
+    public class GerenciadorArquivo
+    {
+        public static void CadastrarImagemProduto(IFormFile file)
+        {
+
+        }
+
+        public static void ExcluirImagemProduto()
+        {
+
+        }
+    }
+}
