efa2591703198c74bf9df0a15f92d5ebfbcf27c8
Francisco Rafael P. da Silva <francisco.silva@fitbank.com.br>
2021-01-07 09:33:23 -0300

    197. Index - Listagem de Produtos - Ordenação - Parte 3

--- LojaVirtual/LojaVirtual/Models/ViewsModels/IndexViewModel.cs
+++ LojaVirtual/LojaVirtual/Models/ViewsModels/IndexViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,5 +11,19 @@ namespace LojaVirtual.Models.ViewsModels
     {
         public NewsletterEmail newsletter { get; set; }
         public IPagedList<Produto> lista { get; set; }
+        public List<SelectListItem> ordenacao
+        {
+            get
+            {
+                return new List<SelectListItem>()
+                {
+                    new SelectListItem("Alfabética", "A"),
+                    new SelectListItem("Menor preço", "ME"),
+                    new SelectListItem("Maior preço", "MA"),
+                };
+            }
+
+            private set { }
+        }
     }
 }
