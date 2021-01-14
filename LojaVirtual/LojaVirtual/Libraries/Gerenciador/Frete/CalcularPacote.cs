5405a6b11b2022127b85cc7546cc201a1a368998
Francisco Rafael P. da Silva <francisco.silva@fitbank.com.br>
2021-01-14 12:23:49 -0300

    Aula 246 -  Carrinho de Compras - Algoritmo Pacote - Parte 1

---
+++ LojaVirtual/LojaVirtual/Libraries/Gerenciador/Frete/CalcularPacote.cs
@@ -0,0 +1,19 @@
+﻿using LojaVirtual.Models;
+using LojaVirtual.Models.ProdutoAgregador;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Libraries.Gerenciador.Frete
+{
+    public class CalcularPacote
+    {
+        public List<Pacote> CalcularPacotesDeProdutos(List<Produto> produtos)
+        {
+            List<Pacote> pacotes = new List<Pacote>();
+            //pacotes.Add();
+            return pacotes;
+        }
+    }
+}
