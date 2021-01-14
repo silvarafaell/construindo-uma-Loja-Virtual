fde027ae8997be017b0faeabd42cb05fcbd0ae4a
Francisco Rafael P. da Silva <francisco.silva@fitbank.com.br>
2021-01-14 16:00:43 -0300

    Aula 248 - Carrinho de Compras - Algoritmo Pacote - Parte 3

--- LojaVirtual/LojaVirtual/Libraries/Gerenciador/Frete/CalcularPacote.cs
+++ LojaVirtual/LojaVirtual/Libraries/Gerenciador/Frete/CalcularPacote.cs
@@ -23,11 +23,23 @@ namespace LojaVirtual.Libraries.Gerenciador.Frete
                     var largura = (pacote.Largura > item.Largura) ? pacote.Largura : item.Largura;
                     var altura = pacote.Altura + item.Altura;
 
+                    var dimensao = comprimento + largura + altura;
+
                     //TODO - Criar novo pacote caso : 30 kg, Dimensao > 200cm
+                    if(peso > 30 || dimensao > 200)
+                    {
+                        pacotes.Add(pacote);
+                        pacote = new Pacote();
+                    }
+
+                    pacote.Peso = pacote.Peso + item.Peso;
+                    pacote.Comprimento = (pacote.Comprimento > item.Comprimento) ? pacote.Comprimento : item.Comprimento;
+                    pacote.Largura = (pacote.Largura > item.Largura) ? pacote.Largura : item.Largura;
+                    pacote.Altura = pacote.Altura + item.Altura;
                 }
             }
+            pacotes.Add(pacote);
 
-            //pacotes.Add();
             return pacotes;
         }
     }
