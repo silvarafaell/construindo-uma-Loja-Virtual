6faddc29ebcc430243206754fa8812e34b7751ba
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-01-17 09:27:17 -0300

    Aula 257 -  Carrinho de Compras WS - Correios - Formatação do Valor

--- LojaVirtual/LojaVirtual/Libraries/Gerenciador/Frete/WSCorreiosCalcularFrete.cs
+++ LojaVirtual/LojaVirtual/Libraries/Gerenciador/Frete/WSCorreiosCalcularFrete.cs
@@ -1,4 +1,5 @@
 ﻿using LojaVirtual.Models;
+using LojaVirtual.Models.Contants;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
@@ -58,11 +59,13 @@ namespace LojaVirtual.Libraries.Gerenciador.Frete
 
             if(resultado.Servicos[0].Erro == "0")
             {
+                var valorLimpo = resultado.Servicos[0].Valor.Replace(".", "");
+                var valorFinal = double.Parse(valorLimpo);
                 return new ValorPrazoFrete()
                 {
-                    TipoFrete = tipoFrete,
+                    TipoFrete = TipoFreteConstant.GetNames(tipoFrete),
                     Prazo = int.Parse(resultado.Servicos[0].PrazoEntrega),
-                    Valor = double.Parse(resultado.Servicos[0].Valor.Replace(".", "").Replace(",", "."))
+                    Valor = valorFinal
                 };
             }
             else if(resultado.Servicos[0].Erro == "008" || resultado.Servicos[0].Erro == "-888")
--- LojaVirtual/LojaVirtual/Models/Contants/TipoFreteConstant.cs
+++ LojaVirtual/LojaVirtual/Models/Contants/TipoFreteConstant.cs
@@ -10,5 +10,16 @@ namespace LojaVirtual.Models.Contants
         public const string SEDEX = "04014";
         public const string SEDEX10 = "40215";
         public const string PAC = "04510";
+
+        public static string GetNames(string code)
+        {
+            foreach (var field in typeof(TipoFreteConstant).GetFields())
+            {
+                if ((string)field.GetValue(null) == code)
+                    return field.Name.ToString();
+            }
+            return "";
+        }
     }
+      
 }
