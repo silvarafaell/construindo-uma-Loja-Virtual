071d0ae92efbea97ca9e42ffbf3e5c5f0078969d
Francisco Rafael P. da Silva <francisco.silva@fitbank.com.br>
2021-01-19 12:25:22 -0300

    Aula 271 - Pagamento - Estrutura HTML - Parte 1

---
+++ LojaVirtual/LojaVirtual/Areas/Colaborador/Controllers/PagamentoController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LojaVirtual.Areas.Colaborador.Controllers
+{
+    public class PagamentoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
